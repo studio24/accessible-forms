fadff3447802c94acf419627fd4e323dc9398dc3
Gareth Trinkwon <gareth@studio24.net>
2026-03-17 13:22:22 +0000

    Added custom HTML block for raw HTML or view insertions into form.

---
+++ src/FieldTypes/CustomHtmlBlockType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Studio24\AccessibleForms\FieldTypes;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Twig\Markup;
+
+class CustomHtmlBlockType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'html' => null,
+            'view' => null
+        ]);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['html'] = $options['html'];
+
+        // Load in template as Twig\Markup object.
+        if (!empty($options['view'])) {
+            $view->vars['html'] = new Markup($options['view'], 'UTF-8');
+        }
+    }
+}
